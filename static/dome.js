2046530a066e2fc506ac6e4d03db6f4123845a59
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-07-28 20:16:32 -0700

    add react

--- static/dome.js
+++ static/dome.js
@@ -1,4 +1,15 @@
 $(function(){
     console.log("Page has loaded");
-    M.AutoInit();
+    if(M){
+        M.AutoInit();
+    }else{
+        console.log("Where's material?");
+        console.warn("Materialize did not load");
+    }
+    if(React){
+        console.log("Hello React World!");
+    }else{
+        console.log("Where are you React?");
+        console.warn("React did not load");
+    }
 })
