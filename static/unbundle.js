92716055c5a28c405fe500ae8158f0cbb8cc1e49
javaarchive <20248577+javaarchive@users.noreply.github.com>
2020-08-03 13:58:10 -0700

    don't reinvent wheel

--- static/unbundle.js
+++ static/unbundle.js
@@ -1,37 +1,6 @@
 import {React} from "React/addons";
 import { Skeleton } from '@material-ui/lab';
+//import {$} from "jquery";
+const $ = require("jquery");
 console.log("bundle :D");
-class RepeatedComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {size: props.size,comp: null};
-    }
-    
-    render(){
-        let list = Array.from(Array(10).keys()).map((number) =>
-  {React.cloneWithProps(this.props.children)});
-        return <span>{list}</span>;
-    }
-    
-    componentDidMount() {
-      // Code to run when component is destoryed -> constructor
-    }
-  
-    componentWillUnmount() {
-      // Componoent dies -> deconstructor
-    }
-    setSize(size){
-      this.setState(function(state, props) {
-        return {
-          size: size
-        };
-      });
-    }
-    setComp(comp){
-        this.setState(function(state, props) {
-          return {
-            comp: comp
-          };
-        });
-      }
-}
+// RIP RepeatedComponent 2020 why did we need that anyway
