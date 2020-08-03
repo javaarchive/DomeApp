a6b778acd3a89e72c7dcb9e2cd0d284cb179187c
javaarchive <20248577+javaarchive@users.noreply.github.com>
2020-08-03 12:19:31 -0700

    trying to do some react state changes

--- static/unbundle.js
+++ static/unbundle.js
@@ -1,3 +1,37 @@
-import {React} from "React";
+import {React} from "React/addons";
 import { Skeleton } from '@material-ui/lab';
 console.log("bundle :D");
+class RepeatedComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {size: props.size,comp: null};
+    }
+    
+    render(){
+        let list = Array.from(Array(10).keys()).map((number) =>
+  {React.cloneWithProps(this.props.children)});
+        return <span>{list}</span>;
+    }
+    
+    componentDidMount() {
+      // Code to run when component is destoryed -> constructor
+    }
+  
+    componentWillUnmount() {
+      // Componoent dies -> deconstructor
+    }
+    setSize(size){
+      this.setState(function(state, props) {
+        return {
+          size: size
+        };
+      });
+    }
+    setComp(comp){
+        this.setState(function(state, props) {
+          return {
+            comp: comp
+          };
+        });
+      }
+}
