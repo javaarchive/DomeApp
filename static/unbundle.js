5ac61a7ccdcd8a1e7186663aa8bd7d88425e4832
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-08-14 11:26:19 -0700

    columns system

--- static/unbundle.js
+++ static/unbundle.js
@@ -4,42 +4,46 @@ import { Skeleton } from "@material-ui/lab";
 //import {$} from "jquery";
 const $ = require("jquery");
 console.log("bundle :D");
+const columnTypes = {
+	"playlists":["Name", "Date", "Songs"]
+}
 // RIP RepeatedComponent 2020 why did we need that anyway
 class ResultView extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {type: props.type};
-    }
-    componentDidMount() {
-      // Code to run when component is destoryed -> constructor
-    }
-  
-    componentWillUnmount() {
-      // Componoent dies -> deconstructor
-    }
-    search(){
+	constructor(props) {
+		super(props);
+		this.state = { type: props.type, col1: i18n.__(columnTypes[props.type][0]), col2: i18n.__(columnTypes[props.type][1]), col3: i18n.__(columnTypes[props.type][2])};
+	}
+	componentDidMount() {
+		// Code to run when component is destoryed -> constructor
+	}
+
+	componentWillUnmount() {
+		// Componoent dies -> deconstructor
+	}
+	search() {
 		//let data = fetch(+"");
-      this.setState(function(state, props) {
-        return {
-          
-        };
-      });
-      
-    }
-    render() {
-      return (
-        <div>
-          <h1>{}</h1>
-        </div>
-      );
-    }
-  }
+		this.setState(function (state, props) {
+			return {};
+		});
+	}
+	render() {
+		return (
+			<>
+				<div class="row">
+					<div class="col s1">Name</div>
+					<div class="col s1">{this.state.col1}</div>
+					<div class="col s1">{this.state.col2}</div>
+					<div class="col s1">{this.state.col3}</div>
+				</div>
+			</>
+		);
+	}
+}
 class PlaylistView extends React.Component {
 	constructor(props) {
 		super(props);
-	this.state = { searchBoxValue: "" };
-	this.mySearchView = <ResultView type="playlist"></ResultView>
-    //this.fetchSearch = this.fetchSearch.bind(this);
+		this.state = { searchBoxValue: "" };
+		//this.fetchSearch = this.fetchSearch.bind(this);
 	}
 	componentDidMount() {
 		// Code to run when component is destoryed -> constructor
@@ -50,32 +54,30 @@ class PlaylistView extends React.Component {
 	}
 	fetchSearch(event) {
 		console.log(event);
-    console.log("Updating Search");
-    let searchValue = event.target.value;
+		console.log("Updating Search");
+		let searchValue = event.target.value;
 		if (event.target.value) {
-			this.setState(function(state, props) {
+			this.setState(function (state, props) {
 				return {
-					searchBoxValue: searchValue
+					searchBoxValue: searchValue,
 				};
 			});
 		}
-  }
-  
+	}
+
 	render() {
 		return (
-      <>
-			<input
-				type="text"
-				id="searchbox-playlists"
-				class="searchbox"
-				onChange={this.fetchSearch.bind(this)}
-				placeholder={i18n.__("Type to search")}
-			/>
-			{this.mySearchView}
-      <div>
-        
-      </div>
-      </>
+			<>
+				<input
+					type="text"
+					id="searchbox-playlists"
+					class="searchbox"
+					onChange={this.fetchSearch.bind(this)}
+					placeholder={i18n.__("Type to search")}
+				/>
+				<ResultView type="playlists"></ResultView>
+				<div></div>
+			</>
 		);
 	}
 }
@@ -85,7 +87,7 @@ views.playlists = <PlaylistView />;
 // Bootstrap code
 if (uiManager) {
 	console.log("Binding to uiManager");
-	uiManager.on("launchview", function(data) {
+	uiManager.on("launchview", function (data) {
 		console.log(data);
 		console.log("Rendering " + data.id);
 		console.log(views[data.id]);
