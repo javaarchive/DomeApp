0d0939114e61138c52e06061ddccf73139bd158b
javaarchive <20248577+javaarchive@users.noreply.github.com>
2020-12-12 17:07:54 -0800

    Work in progress refactor using Material Ui React, also prettiered everything

--- static/unbundle.js
+++ static/unbundle.js
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Core components
 import { Skeleton } from "@material-ui/lab";
-import {PlayerComponent} from "./player";
-const Store = require('electron-store');
+import { Container } from "@material-ui/core";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+// Utilites
+import { makeStyles } from "@material-ui/core/styles";
+
+// Ui icons
+import MenuIcon from "@material-ui/icons/Menu"; // Also called a hamburger
+import StorageIcon from "@material-ui/icons/Storage";
+import Switch from "@material-ui/core/Switch";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+
+// Ui Widgets
+import AppBar from "@material-ui/core/AppBar";
+
+// Reusable Player Componoent
+import { PlayerComponent } from "./player";
+
+const Store = require("electron-store");
 // Settings Loading
-if(!Store){
+if (!Store) {
 	console.warn("NO STORE found");
 }
 const settings = new Store({
@@ -18,30 +39,45 @@ const $ = require("jquery");
 const regeneratorRuntime = require("regenerator-runtime");
 console.log("bundle :D");
 // Localizations
-function formatDuration(seconds){
+function formatDuration(seconds) {
 	let curSecs = seconds;
 	let out = "";
-	if(curSecs > 60*60){
-		out += Math.floor(curSecs/(60*60)) + ":";
+	if (curSecs > 60 * 60) {
+		out += Math.floor(curSecs / (60 * 60)) + ":";
 		curSecs = curSecs % (60 * 60);
 	}
-	out += (Math.floor(curSecs/60).toString().padStart(2,"0")+":"+(curSecs%60).toString().padStart(2,"0"));
+	out +=
+		Math.floor(curSecs / 60)
+			.toString()
+			.padStart(2, "0") +
+		":" +
+		(curSecs % 60).toString().padStart(2, "0");
 	return out;
 }
-
-
 
 // Constants
 const columnTypes = {
 	playlists: ["Name", "Date", "Songs Count"],
 	songs: ["Name", "Artist", "Duration"],
-	albums: ["Name","Last Updated","Songs"]
+	albums: ["Name", "Last Updated", "Songs"],
 };
 const columnProps = {
-	playlists: [item => item.name , item => item.createdAt, item => JSON.parse(item.contents).length],
-	songs: [item => item.name , item => item.artist, (item) => (item.duration)?formatDuration(item.duration):"Unknown"],
-	albums: [item => item.name , item => item.updatedAt, item => JSON.parse(item.contents).length]
-}
+	playlists: [
+		(item) => item.name,
+		(item) => item.createdAt,
+		(item) => JSON.parse(item.contents).length,
+	],
+	songs: [
+		(item) => item.name,
+		(item) => item.artist,
+		(item) => (item.duration ? formatDuration(item.duration) : "Unknown"),
+	],
+	albums: [
+		(item) => item.name,
+		(item) => item.updatedAt,
+		(item) => JSON.parse(item.contents).length,
+	],
+};
 
 let musicServer = "http://localhost:3000"; // NO SLASH!
 function capitlizeFirst(string) {
@@ -54,39 +90,39 @@ function serialize(obj) {
 		str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
 	return str.join("&");
 }
-function calcColClass(cols){
-	return "s"+12/cols;
+function calcColClass(cols) {
+	return "s" + 12 / cols;
 }
 class ResultView extends React.Component {
 	constructor(props) {
-		super(props ); // Deprecated but needed anyway
+		super(props); // Deprecated but needed anyway
 		this.state = {
 			pageIndex: 0,
 			type: props.type,
 			col1: i18n.__(columnTypes[props.type][0]),
 			col2: i18n.__(columnTypes[props.type][1]),
 			col3: i18n.__(columnTypes[props.type][2]),
-			pageData: []
+			pageData: [],
 		};
 		this.search.bind(this)();
 	}
 	shouldComponentUpdate(nextProps) {
 		console.info("Update Request");
-        const queryChanged = this.props.query !== nextProps.query;
-        return queryChanged;
+		const queryChanged = this.props.query !== nextProps.query;
+		return queryChanged;
 	}
-	componentDidUpdate(prevProps){
-		if(this.props.query !== prevProps.query){
+	componentDidUpdate(prevProps) {
+		if (this.props.query !== prevProps.query) {
 			this.search();
 		}
 	}
 	componentDidMount() {
 		// Code to run when component starts
-		console.info("Result View Mounted")
+		console.info("Result View Mounted");
 		this.search();
 		let componentThis = this;
-		this.updateSearchInterval = setInterval(function(){
-			if(componentThis.query){
+		this.updateSearchInterval = setInterval(function () {
+			if (componentThis.query) {
 				componentThis.search.bind(componentThis)();
 			}
 		}, 2500);
@@ -99,36 +135,45 @@ class ResultView extends React.Component {
 	async search() {
 		console.log("Running Search Request");
 		let pageSize = settings.get("pageSize");
-		let resp = await (await fetch(
-			musicServer +
-				"/api/fetch_" +
-				this.state.type +
-				"?" +
-				serialize({
-					limit: pageSize,
-					offset: pageSize * this.state.pageIndex,
-					name: this.props.query + "%",
-				})
-		)).json();
+		let resp = await (
+			await fetch(
+				musicServer +
+					"/api/fetch_" +
+					this.state.type +
+					"?" +
+					serialize({
+						limit: pageSize,
+						offset: pageSize * this.state.pageIndex,
+						name: this.props.query + "%",
+					})
+			)
+		).json();
 		if (resp.status == "ok") {
 			let data = resp.data;
-			console.log("Updating data for "+this.state.query);
+			console.log("Updating data for " + this.state.query);
 			this.setState(function (state, props) {
-				return {pageData: data};
+				return { pageData: data };
 			});
 		}
 	}
 	render() {
-		let colgenerator = function(item){
+		let colgenerator = function (item) {
 			// TODO: Move col sizes to constants
-			return <div className="row wide-item waves-effect waves-light" key={item.id} data-id={item.id} onClick={this.props.onItemClick}>
+			return (
+				<div
+					className="row wide-item waves-effect waves-light"
+					key={item.id}
+					data-id={item.id}
+					onClick={this.props.onItemClick}
+				>
 					<div className="col s6">{columnProps[this.props.type][0](item)}</div>
 					<div className="col s3">{columnProps[this.props.type][1](item)}</div>
 					<div className="col s3">{columnProps[this.props.type][2](item)}</div>
 				</div>
+			);
 		};
 		let comps = this.state.pageData.map(colgenerator.bind(this));
-		
+
 		return (
 			<div className="results-wrapper">
 				<div className="row wide-item">
@@ -136,11 +181,10 @@ class ResultView extends React.Component {
 					<div className="col s3">{this.state.col2}</div>
 					<div className="col s3">{this.state.col3}</div>
 				</div>
-				<div className="results-rows">
-				{comps}
-				</div>
+				<div className="results-rows">{comps}</div>
 				<p>
-		{i18n.__("Showing ")}{this.state.pageData.length} {i18n.__(" items")};
+					{i18n.__("Showing ")}
+					{this.state.pageData.length} {i18n.__(" items")};
 				</p>
 			</div>
 		);
@@ -173,8 +217,8 @@ class PlaylistView extends React.Component {
 			});
 		}
 	}
-	onItemClick(e){
-		console.log("Item Click",e,this);
+	onItemClick(e) {
+		console.log("Item Click", e, this);
 	}
 	render() {
 		return (
@@ -192,7 +236,11 @@ class PlaylistView extends React.Component {
 					query={this.state.searchBoxValue}
 					onItemClick={this.onItemClick.bind(this)}
 				></ResultView>
-				<p>{i18n.__("Current querying ")} {settings.get("pageSize")} {i18n.__(" playlists matching the query ")} {this.state.searchBoxValue}</p>
+				<p>
+					{i18n.__("Current querying ")} {settings.get("pageSize")}{" "}
+					{i18n.__(" playlists matching the query ")}{" "}
+					{this.state.searchBoxValue}
+				</p>
 				<div></div>
 			</>
 		);
@@ -237,37 +285,105 @@ class SongView extends React.Component {
 					onKeyUp={this.fetchSearch.bind(this)}
 					placeholder={i18n.__("Type to search")}
 				/>
-				<ResultView
-					type="songs"
-					query={this.state.searchBoxValue}
-				></ResultView>
-				<p>{i18n.__("Current querying ")} {settings.get("pageSize")} {i18n.__(" songs matching the query ")} {this.state.searchBoxValue}</p>
+				<ResultView type="songs" query={this.state.searchBoxValue}></ResultView>
+				<p>
+					{i18n.__("Current querying ")} {settings.get("pageSize")}{" "}
+					{i18n.__(" songs matching the query ")} {this.state.searchBoxValue}
+				</p>
 				<div></div>
 			</>
 		);
 	}
 }
+// Home View
+// TODO: Populate with intresting things
+class HomeComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	componentDidMount() {
+		// Code to run when component is destoryed -> constructor
+	}
+
+	componentWillUnmount() {
+		// Componoent dies -> deconstructor
+	}
+	stateChange() {
+		this.setState(function (state, props) {
+			return {};
+		});
+	}
+	render() {
+		return (
+			<>
+				<h1>{i18n.__("Hello! This is the default homescreen for now. ")}</h1>
+			</>
+		);
+	}
+}
+// Legacy Views System
+
 let views = {};
 views.playlists = <PlaylistView />;
 views.songs = <SongView />;
+views.homeview = <HomeComponent />;
 window.debug = {};
 window.debug.views = views;
 
-// Bootstrap code
-if (uiManager) {
-	console.log("Binding to uiManager instance ");
-	uiManager.on("launchview", function (data) {
-		console.log(data);
-		console.log("Rendering",data.id);
-		console.log(views[data.id]);
-		console.log(Object.keys(views));
-		ReactDOM.render(<>
-		{views[data.id]}
-		Player goes here
-		<PlayerComponent />
-		</>, document.getElementById("contentview"));
-	}); // Bind to launch view event
-} else {
-	console.error("Ui manager not found");
+// Main Comp
+class Component extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedView: "homeview",
+		};
+	}
+	render() {
+		return <></>;
+	}
+	componentDidMount() {
+		// Code to run when component is destoryed -> constructor
+	}
+
+	componentWillUnmount() {
+		// Componoent dies -> deconstructor
+	}
+	changeView(view) {
+		this.setState(function (state, props) {
+			return {
+				selectedView: view,
+			};
+		});
+	}
 }
+
+// Bootstrap code
+// really odd part i'm learning
+const stylesSet = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	title: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+}));
+$(function () {
+	// TODO: Replace with Vanilla JS to make script size smaller
+
+	ReactDOM.render(
+		<>
+			<div className={classes.root}>
+				<Container maxWidth="sm">{views[data.id]}</Container>
+
+				<PlayerComponent />
+			</div>
+		</>,
+		document.getElementById("root")
+	);
+});
+
 console.log("Player Comp", PlayerComponent);
