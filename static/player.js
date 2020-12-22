d12b1f8ba4f3166e8d1d20fde1133abbb88f82fc
Raymond <raymond.peng07@gmail.com>
2020-12-21 22:21:38 -0800

    hit the formatter

--- static/player.js
+++ static/player.js
@@ -3,78 +3,119 @@ import ReactDOM from "react-dom";
 // Fonts (broken currently)
 // import "fontsource-roboto";
 // Styles
-import styles from "./player.module.css";
-import Typography from "@material-ui/core/Typography";
 
-// React Player to be imported 
+import styles from "./player.module.css";
+// Text Stuff
+import Typography from "@material-ui/core/Typography";
+// Grid Utils
+import Grid from "@material-ui/core/Grid";
+// Widgets
+import Slider from "@material-ui/core/Slider";
+// Get localized functions
+import { localizedFuncs } from "./utils.js";
+
+// React Player to be imported
 // Meant to be reusable in other contexts
 const hasArtist = ["Song"];
 const hasMultipleArtists = ["Album"]; // Playlists are usually user created so they will have a variety of artists
-if(!i18n){
-    try{
-        var i18n = require("i18n");
-    }catch(ex){
-        var i18n = null; // Allow custom instances to be added later.
-    }
+if (!i18n) {
+	try {
+		var i18n = require("i18n");
+	} catch (ex) {
+		var i18n = null; // Allow custom instances to be added later.
+	}
 } // Might already be init
 class PlayerComponent extends React.Component {
-    constructor(props) {
-      super(props); // Deprecated but needed
-      this.state = {
-          itemName: i18n.__("Nothing Playing")
-      };
-    }
-    componentDidMount() {
-      // Code to run when component is destoryed -> constructor
-    }
-  
-    componentWillUnmount() {
-      // Componoent dies -> deconstructor
-    }
-    updateItem(type,params){
-        let properties = {};
-        
-        if("name" in params){
-            properties.itemName = params.name;
-        }
-        if(hasArtist.includes(type)){
-            if("artist" in params){
-                properties.itemMadeBy = params.artist;
-            }
-        }
-        if("duration" in params){
-            properties.duration = params.duration;  
-        }else{
-            properties.duration = null; // Not provided
-        }
-        
-        
-        this.setState(function(state, props) {
-            return properties;
-        });
-        
+	constructor(props) {
+		super(props); // Deprecated but needed
+		let preparedState = {
+			itemName: i18n.__("Nothing Playing"),
+			position: 0,
+			length: 1200,
+			enabled: false,
+			userDragging: false, // Do not update while user is dragging
+		};
+		if (props.name) {
+			preparedState["name"] = props.name;
+			preparedState["enabled"] = true;
+		}
+		this.state = preparedState;
+	}
+	componentDidMount() {
+		// Code to run when component is destoryed -> constructor
+	}
 
-    }
-    updateDuration(time){
-        // Sometimes duration can be found afterwards
-        this.setState(function(state, props) {
-            return {itemDuration: time};
-        });
-    }
-    render() {
-      return (
-        <>
-        <div className="player">
-            <span className={styles.playerTitle}>
-                <Typography variant="h5">{this.state.itemName}</Typography>
-            </span>
-            <span className={styles.playerItemMadeBy}>
-                <Typography variant="h6">{this.state.itemMadeBy}</Typography>
-            </span>
-        </div>
-        </>
-      );
-    }
-  }
-console.log("Imported styles",styles);
-export {PlayerComponent};
+	componentWillUnmount() {
+		// Componoent dies -> deconstructor
+	}
+	updateItem(type, params) {
+		let properties = {};
+
+		if ("name" in params) {
+			properties.itemName = params.name;
+		}
+		if (hasArtist.includes(type)) {
+			if ("artist" in params) {
+				properties.itemMadeBy = params.artist;
+			}
+		}
+		if ("duration" in params) {
+			properties.duration = params.duration;
+		} else {
+			properties.duration = null; // Not provided
+		}
+
+		this.setState(function (state, props) {
+			return properties;
+		});
+	}
+	changePos(ev, newVal) {
+		console.log("Position changed to", newVal);
+		this.setState(function (state, props) {
+			return { position: newVal };
+		});
+	}
+	updateDuration(time) {
+		// Sometimes duration can be found afterwards
+		this.setState(function (state, props) {
+			return { itemDuration: time };
+		});
+	}
+	render() {
+		return (
+			<>
+				<div className="player">
+					<Grid container spacing={2}>
+						<Grid item xs={10}>
+							<div className="playback-progress">
+								<Slider
+									value={this.state.position}
+									onChange={this.changePos.bind(this)}
+									aria-labelledby="continuous-slider"
+									min={0}
+									max={this.state.length}
+									disabled={!this.state.enabled}
+								/>
+							</div>
+						</Grid>
+						<Grid item xs={2}>
+							{this.state.enabled
+								? localizedFuncs[i18n.getLocale()].formatDuration(
+										this.state.position
+								  )
+								: i18n.__("Idle Duration")}
+						</Grid>
+					</Grid>
+					<span className={styles.playerTitle}>
+						<Typography variant="h5">{this.state.itemName}</Typography>
+					</span>
+					<span className={styles.playerItemMadeBy}>
+						<Typography variant="h6">{this.state.itemMadeBy}</Typography>
+					</span>
+				</div>
+			</>
+		);
+	}
+}
+console.log("Imported styles", styles);
+export { PlayerComponent };
