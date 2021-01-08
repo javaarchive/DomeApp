75c8ee1d98b66458b716c2f556d98b0856402e35
javaarchive <20248577+javaarchive@users.noreply.github.com>
2021-01-08 13:08:32 -0800

    prefferedplayersystem

--- modules/contenthandlers/youtube.js
+++ modules/contenthandlers/youtube.js
@@ -1,10 +1,14 @@
 const prefix = "youtube://";
 class YoutubeContentHandler{
+    constructor(){
+        this.prefferedPlayerKey = "youtubePlayerMethod";
+    }
     canHandle(uri){
         return uri.startsWith(PREFIX);
     }
     getPlaybackMethod(){
         return require("../youtube_embed_iframe");
     }
+    
 }
 module.exports = YoutubeContentHandler;
